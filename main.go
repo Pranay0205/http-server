935b96db7a32f8b502d0c86b8913226c813a36e0
Pranay0205 <pranayghuge@gmail.com>
2025-09-16 22:31:57 -0400

    Refactor getLinesChannel function to improve file reading and error handling

--- main.go
+++ main.go
@@ -1,11 +1,56 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
 )
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+
+	channel := make(chan string)
+
+	go func() {
+		currentLine := ""
+		defer file.Close()
+		for {
+			buffer := make([]byte, 8)
+
+			readBytes, err := file.Read(buffer)
+
+			if err != nil {
+				if currentLine != "" {
+					channel <- currentLine
+				}
+				close(channel)
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("End of the line:%s", err.Error())
+				return
+			}
+
+			data := currentLine + string(buffer[:readBytes])
+			parts := strings.Split(data, "\n")
+
+			for i := 0; i < len(parts)-1; i++ {
+				channel <- parts[i]
+			}
+
+			currentLine = parts[len(parts)-1]
+
+		}
+
+	}()
+
+	return channel
+}
 
 const inputFilePath = "message.txt"
 
@@ -15,33 +60,11 @@ func main() {
 		log.Fatalf("Error opening file: %s", err)
 		return
 	}
-	defer file.Close()
 
-	fmt.Printf("Reading data from %s\n", inputFilePath)
-	fmt.Println("=====================================")
+	channelVariable := getLinesChannel(file)
 
-	currentLine := ""
-	for {
-		buffer := make([]byte, 8)
-
-		readBytes, err := file.Read(buffer)
-
-		if readBytes == 0 {
-			fmt.Printf("Unable to read the file: %s", err)
-			break
-		}
-
-		data := currentLine + string(buffer[:readBytes])
-		parts := strings.Split(data, "\n")
-
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s\n", parts[i])
-		}
-
-		currentLine = parts[len(parts)-1]
+	for line := range channelVariable {
+		fmt.Printf("read: %s\n", line)
 	}
 
-	if currentLine != "" {
-		fmt.Printf("read: %s\n", currentLine)
-	}
 }
