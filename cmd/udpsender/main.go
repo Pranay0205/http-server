bddf0d0b513c61d914caea6f5029ff4ee77be160
Pranay0205 <pranayghuge@gmail.com>
2025-09-17 14:00:31 -0400

    feat: Implement UDP sender with user input handling

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const addr = "localhost:42069"
+
+func main() {
+	raddr, err := net.ResolveUDPAddr("udp", addr)
+
+	if err != nil {
+		log.Fatalf("😭 error listening for UDP traffic:", err)
+		return
+	}
+
+	fmt.Printf("✅ UDP Addr is ready on %s\n\n", raddr)
+
+	conn, err := net.DialUDP(raddr.Network(), nil, raddr)
+	if err != nil {
+		log.Fatalf("😭 error dialing UDP: %s\n", err)
+	}
+	defer conn.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("> ")
+
+		text, err := reader.ReadString('\n')
+
+		if err != nil {
+			log.Fatalf("😭 Error reading the input: %s\n", err)
+			return
+		}
+
+		_, err = conn.Write([]byte(text))
+		if err != nil {
+			log.Fatalf("😭 Error sending the message: %s\n", err)
+		}
+	}
+
+}
